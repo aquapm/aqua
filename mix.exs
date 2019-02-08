eca8123f6e04807174f4718d64f154ca256fba28
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-02-08 23:10:55 +0200

    Fix mix

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Aqua.MixProject do
       aliases: aliases(),
       docs: docs(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
