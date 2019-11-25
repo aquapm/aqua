5185b0aaf6d0ee4f35a3c51ca5d762bb2d6202fa
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-11-25 14:11:11 +0200

    XDG speed up

--- lib/util/xdg.ex
+++ lib/util/xdg.ex
@@ -1,13 +1,14 @@
 defmodule Aqua.XDG do
   @data [
-    data_home: Path.join([System.user_home, ".local", "share"]),
-    config_home: Path.join([System.user_home, ".config"]),
-    cache_home: Path.join([System.user_home, ".cache"])
+    data_home: Path.join([System.user_home(), ".local", "share"]),
+    config_home: Path.join([System.user_home(), ".config"]),
+    cache_home: Path.join([System.user_home(), ".cache"])
   ]
 
   for {name, path} <- @data do
     def unquote(name)() do
-      System.get_env(String.upcase("xdg_#{unquote(name)}")) || unquote(path)
+      # System.get_env(String.upcase("xdg_#{unquote(name)}")) || unquote(path)
+      System.get_env(unquote(String.upcase("xdg_#{name}"))) || unquote(path)
     end
   end
 end
